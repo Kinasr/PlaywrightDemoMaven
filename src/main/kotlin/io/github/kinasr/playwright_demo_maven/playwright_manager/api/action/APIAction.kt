c44ab4502c56a0bb4b31bccc0b1e5837bb67e270
kinasr <kinasr95@gmail.com>
2025-08-19 17:47:04 +0300

    api attachment

--- src/main/kotlin/io/github/kinasr/playwright_demo_maven/playwright_manager/api/action/APIAction.kt
+++ src/main/kotlin/io/github/kinasr/playwright_demo_maven/playwright_manager/api/action/APIAction.kt
@@ -2,12 +2,13 @@ package io.github.kinasr.playwright_demo_maven.playwright_manager.api.action
 
 import com.google.gson.Gson
 import com.microsoft.playwright.impl.TargetClosedError
-import com.microsoft.playwright.options.RequestOptions
 import io.github.kinasr.playwright_demo_maven.config.Config
-import io.github.kinasr.playwright_demo_maven.playwright_manager.api.model.APIMethod
-import io.github.kinasr.playwright_demo_maven.playwright_manager.api.model.APIResult
 import io.github.kinasr.playwright_demo_maven.playwright_manager.api.manager.APIRequestManager
+import io.github.kinasr.playwright_demo_maven.playwright_manager.api.model.APIMethod
+import io.github.kinasr.playwright_demo_maven.playwright_manager.api.model.APIRequest
+import io.github.kinasr.playwright_demo_maven.playwright_manager.api.model.APIResult
 import io.github.kinasr.playwright_demo_maven.playwright_manager.result
+import io.github.kinasr.playwright_demo_maven.utils.ifNull
 import io.github.kinasr.playwright_demo_maven.utils.logger.PlayLogger
 import io.github.kinasr.playwright_demo_maven.utils.report.Report
 import io.github.kinasr.playwright_demo_maven.utils.report.model.AttachmentType
@@ -22,34 +23,38 @@ class APIAction(
     private val validationBuilder: ValidationBuilder
 ) {
 
-    fun <T> send(
+    private fun <T> send(
         method: APIMethod,
         endpoint: String,
         bodyType: Class<T>,
-        options: (RequestOptions.() -> Unit) = {}
+        options: (APIRequest.() -> Unit) = {}
     ): APIResult<T> {
+        APIRequest(APIMethod.GET)
         requestManager.use { manager ->
-            val contextOptions = RequestOptions.create().apply {
+            val requestOptions = APIRequest(method).apply {
                 options()
-                this.setMethod(method.str)
-                this.setMaxRetries(config.api.maxRetries)
+                this.maxRetries = this.maxRetries.ifNull { config.api.maxRetries }
             }
-            
+
             fun executeRequest(): APIResult<T> {
-                return manager.request.fetch(endpoint, contextOptions)
+                return manager.request.fetch(
+                    endpoint,
+                    requestOptions.toRequestOptions(jsonConverter)
+                )
                     .result(bodyType, jsonConverter, validationBuilder)
             }
 
+            val jsonRequest = requestOptions.toPrettyJsonString(jsonConverter)
             val logMessage = "Sending request to: ${method.str}: ${manager.baseURL}$endpoint"
             logger.info { logMessage }
-            logger.apiDebug { "Request options: $contextOptions" }
+            logger.apiDebug { "Request options: $jsonRequest" }
             val step = report.step(logMessage)
                 .attach(
                     "Request",
-                    contextOptions.toString().toByteArray(),
+                    jsonRequest.toByteArray(),
                     AttachmentType.JSON
                 )
-            
+
             return try {
                 executeRequest()
             } catch (_: TargetClosedError) {
@@ -62,6 +67,7 @@ class APIAction(
                 throw e
             }.also { result ->
                 logger.info { "Request sent successfully with status code: ${result.statusCode}" }
+                logger.apiDebug { "Response: $result" }
                 step.parameter("Status Code", result.status)
                 step.attach("Response", result.text.toByteArray(), AttachmentType.JSON)
                 step.passed()
@@ -69,59 +75,59 @@ class APIAction(
         }
     }
 
-    fun send(
+    private fun send(
         method: APIMethod,
         endpoint: String,
-        options: (RequestOptions.() -> Unit) = {}
+        options: (APIRequest.() -> Unit) = {}
     ): APIResult<String> {
         return send(method, endpoint, String::class.java, options)
     }
-    
-    fun get(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun get(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.GET, endpoint, bodyType, options)
     }
-    
-    fun get(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun get(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return get(endpoint, String::class.java, options)
     }
-    
-    fun post(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun post(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.POST, endpoint, bodyType, options)
     }
-    
-    fun post(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun post(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return post(endpoint, String::class.java, options)
-    }   
-    
-    fun put(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+    }
+
+    fun put(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.PUT, endpoint, bodyType, options)
     }
-    
-    fun put(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun put(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return put(endpoint, String::class.java, options)
-    }   
-    
-    fun delete(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+    }
+
+    fun delete(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.DELETE, endpoint, bodyType, options)
     }
-    
-    fun delete(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun delete(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return delete(endpoint, String::class.java, options)
     }
-    
-    fun patch(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun patch(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.PATCH, endpoint, bodyType, options)
     }
-    
-    fun patch(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun patch(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return patch(endpoint, String::class.java, options)
     }
-    
-    fun head(endpoint: String, bodyType: Class<String>, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun head(endpoint: String, bodyType: Class<String>, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return send(APIMethod.HEAD, endpoint, bodyType, options)
     }
-    
-    fun head(endpoint: String, options: (RequestOptions.() -> Unit) = {}): APIResult<String> {
+
+    fun head(endpoint: String, options: (APIRequest.() -> Unit) = {}): APIResult<String> {
         return head(endpoint, String::class.java, options)
     }
 }
--- src/test/kotlin/io/github/kinasr/playwright_demo_maven/tests/DemoTest.kt
+++ src/test/kotlin/io/github/kinasr/playwright_demo_maven/tests/DemoTest.kt
@@ -1,15 +1,11 @@
 package io.github.kinasr.playwright_demo_maven.tests
 
-import com.microsoft.playwright.APIRequest
 import io.github.kinasr.playwright_demo_maven.aut.api.TaskAPICollection
 import io.github.kinasr.playwright_demo_maven.di.PlaywrightTestScope
-import io.github.kinasr.playwright_demo_maven.playwright_manager.api.manager.APIRequestManager
 import org.junit.jupiter.api.Test
-import org.koin.core.parameter.parametersOf
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.test.KoinTest
-import org.koin.test.get
 import java.util.*
 
 class DemoTest : KoinTest {
@@ -27,7 +23,7 @@ class DemoTest : KoinTest {
         taskAPICollection.get()
             .validate().isForbidden()
             .then.assert()
-        
+
         taskAPICollection.get()
     }
 }
